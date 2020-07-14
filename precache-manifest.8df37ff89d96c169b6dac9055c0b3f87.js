1cc62d64bfa68ab2b87dfd44471b9daff067c6b2
Amirtha <amirtha4501@gmail.com>
2020-07-14 20:13:33 +0530

    Updates

---
+++ precache-manifest.8df37ff89d96c169b6dac9055c0b3f87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c3c9a20d385eb47bf1700bef699eed9",
+    "url": "/QuizMaster/index.html"
+  },
+  {
+    "revision": "366d7738b9c1e47ea322",
+    "url": "/QuizMaster/static/js/2.532302c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/QuizMaster/static/js/2.532302c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "180816a70a1188ca0013",
+    "url": "/QuizMaster/static/js/main.99bcc473.chunk.js"
+  },
+  {
+    "revision": "657d2cc4440a62f9060f",
+    "url": "/QuizMaster/static/js/runtime-main.79d29184.js"
+  },
+  {
+    "revision": "7e7e53e52bb74f9139f2b444138562a7",
+    "url": "/QuizMaster/static/media/droplets.7e7e53e5.jpg"
+  }
+]);
